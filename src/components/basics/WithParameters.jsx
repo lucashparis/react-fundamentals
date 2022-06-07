d6bef6c2e5d8a39da51808257aeafc4453209caf
Lucas Paris <lucashenriqueparis@gmail.com>
2022-06-07 00:51:27 -0300

    Creating components with parameters

---
+++ src/components/basics/WithParameters.jsx
@@ -0,0 +1,8 @@
+export default function WithParameters(props){
+    return (
+        <div>
+            <h2>{props.titulo}</h2>
+            <h3>{props.subtitulo}</h3>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import ReactDom from 'react-dom'
 import React from 'react'
 import './index.css'
 
-import Primeiro from './components/basics/First'
+import First from './components/basics/First'
+import WithParameters from './components/basics/WithParameters'
 const el = document.getElementById('root')
 ReactDom.render(
-    <div>
-        <Primeiro></Primeiro>
+    <div id="app">
+        <First></First>
+        <WithParameters 
+            titulo="Two Component"
+            subtitulo="Sub Title"
+        >
+        </WithParameters>
     </div>, 
     el
 )
