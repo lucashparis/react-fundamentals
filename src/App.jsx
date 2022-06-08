5ed8ce3471909a012b664d0fe24e6ea22afa70e7
Lucas Paris <lucashenriqueparis@gmail.com>
2022-06-07 22:51:51 -0300

    Challenge number random

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import First from './components/basics/First';
 import WithParameters from './components/basics/WithParameters';
 import Fragment from './components/basics/Fragment';
+import Random from './components/basics/Random';
 
 export default function App(props) {
     return (
     <div id="app">
         <h1>React fundamentals</h1>
+        <hr />
+        <Random
+            vlMin={1}
+            vlMax={100}
+        />
+        <hr />
         <Fragment/> 
         <WithParameters 
             titulo="Two Component"
---
+++ src/components/basics/Random.jsx
@@ -0,0 +1,14 @@
+export default function Random(props) {
+    console.log('props', props);
+    const {vlMin, vlMax} = props
+    const vlRandom = parseInt(Math.floor(Math.random() * (vlMax - vlMin + 1)) + vlMin);
+
+    return (
+        <div>
+            <h1>Number Random</h1>
+            <p>Number Min: {vlMin}</p>
+            <p>Number Max: {vlMax}</p> 
+            <p>Number Random is: {vlRandom} </p>
+        </div>
+    )
+}
