c46f6cf94a942693511dbf9b20301034c429079b
Lucas Paris <lucashenriqueparis@gmail.com>
2022-06-18 20:18:51 -0300

    Remove react deprecation alert

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import ReactDom from 'react-dom';
 import React from 'react';
+import ReactDOM from "react-dom/client";
 import './index.css';
 
 import App from './App';
 
-ReactDom.render(
-    <App/>, 
-    document.getElementById('root')
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
+
