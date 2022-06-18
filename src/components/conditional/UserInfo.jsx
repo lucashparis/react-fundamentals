79870c00ea671a8029c9c1562c31c09837a5a78d
Lucas Paris <lucashenriqueparis@gmail.com>
2022-06-17 21:17:09 -0300

    Conditional structure

--- src/components/conditional/UserInfo.jsx
+++ src/components/conditional/UserInfo.jsx
@@ -1,12 +1,26 @@
-import If from './If'
+import If, { Else } from './If'
 
 export default function UserInfo(props){
     const user = props.user || {};
     return (
         <div>
-            <If test = {user && user.name}>
+            {/* <If test = {user && user.name}>
                 Welcome {user.name}
             </If>
+            <If test = {!user && !user.name}>
+                Welcome Brother
+            </If>
+            <If test = {!user && !user.name}>
+                Welcome Brother
+            </If> */}
+
+            <If test = {user && user.name}>
+                Welcome {user.name}
+                <Else>
+                    Welcome Brother
+                </Else>
+            </If>
+            
         </div>
     );
 };
